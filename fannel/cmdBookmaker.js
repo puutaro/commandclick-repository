278e164a31984e4ebea69dde76ae67d60f98697c
puutaro <hoperstep@gmail.com>
2023-03-21 14:11:11 +0900

    add  bookmaker fannel

---
+++ fannel/cmdBookmaker.js
@@ -0,0 +1,51 @@
+
+
+/// LABELING_SECTION_START
+// Bookmark fannel @puutaro
+// 	play -> launch bookmark site
+//		- recent visit site url show in "Save title"
+//		- change item  order by drag and drop 
+//		- delete item  by doragging to another area(no item area).
+// 	bookmarkName 
+//		- Input or select bookmark list file name
+//		- prefix must be "book" ex) "bookSites"
+// --
+// --
+// bellow setting variable main line up
+// * EditExecute is edit mode change
+//	- NO is normal edit
+//	- ONCE is one time edit and execute
+//	- ALWAYD is always edit and execute
+// * onUpdateLastModify is how updating file last modified status when executing
+//  - ON: update this (default)
+//  - OFF: no update this
+/// LABELING_SECTION_END
+
+
+/// SETTING_SECTION_START
+editExecute="ALWAYS"
+onUpdateLastModify="ON"
+setVariableType="bookmarkName:EFCB=book"
+setVariableType=""
+scriptFileName="cmdBookmaker.js"
+/// SETTING_SECTION_END
+
+
+/// CMD_VARIABLE_SECTION_START
+bookmarkName="bookSites"
+/// CMD_VARIABLE_SECTION_END
+
+
+/// Please write bellow with javascript
+
+
+const EDIT_FILE_PATH = "${01}/" + tubePlayListName;
+const APP_URL_HISTORY_PATH="${01}/system/url/cmdclickUrlHistory";
+
+
+jsIntent.launchEditSite(
+	EDIT_FILE_PATH,
+	APP_URL_HISTORY_PATH,
+	"true",
+	"true"
+);
