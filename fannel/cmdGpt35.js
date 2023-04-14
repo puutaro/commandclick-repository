a5aeff251ba090916ae1747a1a11e065889ea6cc
puutaro <hoperstep@gmail.com>
2023-04-15 00:39:30 +0900

    enforce gpt35

--- fannel/cmdGpt35.js
+++ fannel/cmdGpt35.js
@@ -3,6 +3,7 @@
 /// LABELING_SECTION_START
 // GPT3.5 free client @puutaro
 // * TXT_TO_CLIP -> copy text to clipboard and launch gpt35 site
+// * REMOVE_TEXT -> remove request from list
 // bellow setting variable main line up
 // * EditExecute is edit mode change
 //	- NO is normal edit
@@ -22,52 +23,16 @@ terminalSizeType="LONG"
 onUrlHistoryRegister="OFF"
 terminalFontZoom="130"
 setVariableType="LAUNCH_GPT35:RO="
-setVariableType="TXT_TO_CLIP1:EB=::TermLong::jsf '${0}' TXT_TO_CLIP1"
-setVariableType="TXT_TO_CLIP2:EB=::TermLong::jsf '${0}' TXT_TO_CLIP2"
-setVariableType="TXT_TO_CLIP3:EB=::TermLong::jsf '${0}' TXT_TO_CLIP3"
-setVariableType="TXT_TO_CLIP4:EB=::TermLong::jsf '${0}' TXT_TO_CLIP4"
-setVariableType="TXT_TO_CLIP5:EB=::TermLong::jsf '${0}' TXT_TO_CLIP5"
-setVariableType="TXT_TO_CLIP6:EB=::TermLong::jsf '${0}' TXT_TO_CLIP6"
-setVariableType="TXT_TO_CLIP7:EB=::TermLong::jsf '${0}' TXT_TO_CLIP7"
-setVariableType="TXT_TO_CLIP8:EB=::TermLong::jsf '${0}' TXT_TO_CLIP8"
-setVariableType="TXT_TO_CLIP9:EB=::TermLong::jsf '${0}' TXT_TO_CLIP9"
-setVariableType="TXT_TO_CLIP10:EB=::TermLong::jsf '${0}' TXT_TO_CLIP10"
-setVariableType="TXT_TO_CLIP11:EB=::TermLong::jsf '${0}' TXT_TO_CLIP11"
-setVariableType="TXT_TO_CLIP12:EB=::TermLong::jsf '${0}' TXT_TO_CLIP12"
-setVariableType="TXT_TO_CLIP13:EB=::TermLong::jsf '${0}' TXT_TO_CLIP13"
-setVariableType="TXT_TO_CLIP14:EB=::TermLong::jsf '${0}' TXT_TO_CLIP14"
-setVariableType="TXT_TO_CLIP15:EB=::TermLong::jsf '${0}' TXT_TO_CLIP15"
-setVariableType="TXT_TO_CLIP16:EB=::TermLong::jsf '${0}' TXT_TO_CLIP16"
-setVariableType="TXT_TO_CLIP17:EB=::TermLong::jsf '${0}' TXT_TO_CLIP17"
-setVariableType="TXT_TO_CLIP18:EB=::TermLong::jsf '${0}' TXT_TO_CLIP18"
-setVariableType="TXT_TO_CLIP19:EB=::TermLong::jsf '${0}' TXT_TO_CLIP19"
-setVariableType="TXT_TO_CLIP20:EB=::TermLong::jsf '${0}' TXT_TO_CLIP20"
+setVariableType="TXT_TO_CLIP:LCBB=${01}/cmdGpt35Dir/reqList|::TermLong::jsf '${0}' clip"
+setVariableType="REMOVE_TEXT:LCBB=${01}/cmdGpt35Dir/reqList|jsf '${0}' remove"
 scriptFileName="cmdGpt35.js"
 /// SETTING_SECTION_END
 
 
 /// CMD_VARIABLE_SECTION_START
 LAUNCH_GPT35="https://huggingface.co/spaces/kunishou/Rapid-GPT"
-TXT_TO_CLIP1=""
-TXT_TO_CLIP2=""
-TXT_TO_CLIP3=""
-TXT_TO_CLIP4=""
-TXT_TO_CLIP5=""
-TXT_TO_CLIP6=""
-TXT_TO_CLIP7=""
-TXT_TO_CLIP8=""
-TXT_TO_CLIP9=""
-TXT_TO_CLIP10=""
-TXT_TO_CLIP11=""
-TXT_TO_CLIP12=""
-TXT_TO_CLIP13=""
-TXT_TO_CLIP14=""
-TXT_TO_CLIP15=""
-TXT_TO_CLIP16=""
-TXT_TO_CLIP17=""
-TXT_TO_CLIP18=""
-TXT_TO_CLIP19=""
-TXT_TO_CLIP20=""
+TXT_TO_CLIP=""
+REMOVE_TEXT=""
 /// CMD_VARIABLE_SECTION_END
 
 
@@ -77,16 +42,23 @@ TXT_TO_CLIP20=""
 
 let args = jsArgs.get().split("\t");
 const firstArgs = args.at(0);
+const REQ_LIST_DIR_PATH = "${01}/cmdGpt35Dir";
+const REQ_LIST_FILE_PATH = `${REQ_LIST_DIR_PATH}/reqList`;
 const launchGpt35Arg = "";
-const pasteArg = "paste";
+const clipArg = "clip";
+const removeArg = "remove";
+const escapeCharHyphen = "-";
 
 
-switch(true){
-	case firstArgs == "":
+switch(firstArgs){
+	case launchGpt35Arg:
 		execLaunchGpt35();
 		break;
-	case firstArgs.includes("TXT_TO_CLIP"):
-		pasteText(eval(firstArgs));
+	case clipArg:
+		clipText(TXT_TO_CLIP);
+		break;
+	case removeArg:
+		removeFromList(REMOVE_TEXT);
 		break;
 };
 
@@ -95,8 +67,86 @@ function execLaunchGpt35(){
 	jsUrl.loadUrl(LAUNCH_GPT35);
 };
 
-function pasteText(text){
-	jsUtil.copyToClipboard(text, 10);
+
+function clipText(text){
+	const clipText = text.trim();
+	jsUtil.copyToClipboard(clipText, 10);
 	jsToast.short("copy ok");
+	updateListFileCon(clipText);
 	execLaunchGpt35();
+};
+
+function updateListFileCon(clipText){
+	jsFileSystem.createDir(
+		REQ_LIST_DIR_PATH
+	);
+	let currentListCons = jsFileSystem.readLocalFile(
+			REQ_LIST_FILE_PATH
+		).split("\n");
+	if(
+		clipText === escapeCharHyphen
+	) return;
+	const inInclude = currentListCons.find(
+		function(req){
+			return req.trim() === clipText;
+		}
+	);
+	if(inInclude) return;
+	const updateListConSource = clipText + 
+		"\n" + 
+		jsFileSystem.readLocalFile(
+			REQ_LIST_FILE_PATH
+		);
+	const updateListCon = updateListConSource
+		.split("\n")
+		.filter(
+			function(req){
+				const trimReq = req.trim();
+				return trimReq !== "" 
+					&& trimReq !== escapeCharHyphen;
+			}
+		).join("\n");
+	jsFileSystem.writeLocalFile(
+        REQ_LIST_FILE_PATH,
+        updateListCon
+	);
+};
+
+function removeFromList(reqText){
+	jsFileSystem.createDir(
+		REQ_LIST_DIR_PATH
+	);
+	let currentListCons = jsFileSystem.readLocalFile(
+			REQ_LIST_FILE_PATH
+		).split("\n");
+	const trimRequest = reqText.trim();
+	if(
+		trimRequest == escapeCharHyphen
+	) return;
+	const inInclude = currentListCons.find(
+		function(req){
+			return req.trim() === trimRequest;
+		}
+	);
+	if(!inInclude) return;
+	const updateListCon = jsFileSystem.readLocalFile(
+		REQ_LIST_FILE_PATH
+	)
+		.split("\n")
+		.filter(
+			function(req){
+				const trimReq = req.trim();
+				return trimReq !== "" 
+					&& trimReq !== escapeCharHyphen
+					&& trimReq !== trimRequest;
+			}
+		).join("\n");
+	jsFileSystem.writeLocalFile(
+        REQ_LIST_FILE_PATH,
+        updateListCon
+	);
+	jsIntent.launchShortcut(
+        "${01}",
+        "${02}"
+    );
 };
