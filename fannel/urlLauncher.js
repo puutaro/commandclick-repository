524cd4281719cb6210569ca6ae1aa4c5d4964ff8
puutaro <hoperstep@gmail.com>
2023-04-15 22:31:45 +0900

    enforce urlLaunch

--- fannel/urlLauncher.js
+++ fannel/urlLauncher.js
@@ -20,12 +20,15 @@ editExecute="ALWAYS"
 terminalSizeType="LONG"
 onUrlHistoryRegister="OFF"
 terminalFontZoom="130"
-scriptFileName="urlLauncher.js"
+setVariableType="LAUNCH_URL:LCB=${01}/urlLauncherDir/launchUrlList"
+setVariableType="REMOVE_LAUNCH_URL:LCBB=${01}/urlLauncherDir/launchUrlList|jsf '${0}' REMOVE_LAUNCH_URL"
+scriptFileName="cmdGpt35.js"
 /// SETTING_SECTION_END
 
 
 /// CMD_VARIABLE_SECTION_START
 LAUNCH_URL=""
+REMOVE_LAUNCH_URL=""
 /// CMD_VARIABLE_SECTION_END
 
 
@@ -35,12 +38,29 @@ LAUNCH_URL=""
 
 let args = jsArgs.get().split("\t");
 const firstArgs = args.at(0);
-const launchUrlArg = "";
+const REQ_LIST_DIR_PATH = "${01}/urlLauncherDir";
+const LAUNCH_URL_LIST_FILE_PATH = `${REQ_LIST_DIR_PATH}/launchUrlList`;
+const REQ_LIST_FILE_PATH = `${REQ_LIST_DIR_PATH}/reqList`;
+const normalArg = "";
+const removeLaunchUrlArg = "REMOVE_LAUNCH_URL";
+const escapeCharHyphen = "-";
 
 
 switch(firstArgs){
-	case launchUrlArg:
+	case normalArg:
+		updateListFileCon(
+			REQ_LIST_DIR_PATH,
+			LAUNCH_URL_LIST_FILE_PATH,
+			LAUNCH_URL
+		);
 		execUrlLaunch();
+		break;
+	case removeLaunchUrlArg:
+		removeFromList(
+			REQ_LIST_DIR_PATH,
+			LAUNCH_URL_LIST_FILE_PATH,
+			LAUNCH_URL
+		);
 		break;
 };
 
@@ -50,3 +70,107 @@ function execUrlLaunch(){
 };
 
 
+function updateListFileCon(
+	searchListDirPath,
+	searchListFilePath,
+	searchText
+){
+	let currentListCons = readWithHyphenCheck(
+		searchListDirPath,
+		searchListFilePath,
+		searchText
+	);
+	if(
+		currentListCons.length === 0
+	) return;
+	const inInclude = currentListCons.find(
+		function(req){
+			return req.trim() === searchText;
+		}
+	);
+	if(inInclude) return;
+	const updateListConSource = searchText + 
+		"\n" + 
+		jsFileSystem.readLocalFile(
+			searchListFilePath
+		);
+	const updateListCon = updateListConSource
+		.split("\n")
+		.filter(
+			function(req){
+				const trimReq = req.trim();
+				return trimReq !== "" 
+					&& trimReq !== escapeCharHyphen;
+			}
+		).join("\n");
+	jsFileSystem.writeLocalFile(
+        searchListFilePath,
+        updateListCon
+	);
+};
+
+function removeFromList(
+	searchListDirPath,
+	searchListFilePath,
+	searchText
+){
+	let currentListCons = readWithHyphenCheck(
+		searchListDirPath,
+		searchListFilePath,
+		searchText
+	);
+	if(
+		currentListCons.length === 0
+	) return;
+	const trimRequest = searchText.trim();
+	const inInclude = currentListCons.find(
+		function(req){
+			return req.trim() === trimRequest;
+		}
+	);
+	if(!inInclude) {
+		jsToast.short(`no exist: ${searchText}`);
+		return;
+	};
+	const updateListCon = jsFileSystem.readLocalFile(
+		searchListFilePath
+	)
+		.split("\n")
+		.filter(
+			function(req){
+				const trimReq = req.trim();
+				return trimReq !== "" 
+					&& trimReq !== escapeCharHyphen
+					&& trimReq !== trimRequest;
+			}
+		).join("\n");
+	jsFileSystem.writeLocalFile(
+        searchListFilePath,
+        updateListCon
+	);
+	jsIntent.launchShortcut(
+        "${01}",
+        "${02}"
+    );
+};
+
+
+function readWithHyphenCheck(
+	searchListDirPath,
+	searchListFilePath,
+	searchText
+){
+	jsFileSystem.createDir(
+		searchListDirPath
+	);
+	let currentListCons = jsFileSystem.readLocalFile(
+			searchListFilePath
+		).split("\n");
+	const trimSearchText = searchText.trim();
+	if(
+		trimSearchText == escapeCharHyphen
+	) {
+		return [];
+	};
+	return currentListCons;
+};
