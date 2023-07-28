64fb55fc9ea103413b278b0d3c177726bb0b7d23
puutaro <hoperstep@gmail.com>
2023-07-28 16:32:17 +0900

    add fannel

---
+++ fannel/askGpt35.js
@@ -0,0 +1,15 @@
+
+
+/// LABELING_SECTION_START
+//  You can ask gpt35 @puutaro
+/// LABELING_SECTION_END
+
+
+/// SETTING_SECTION_START
+scriptFileName="askGpt35.js"
+/// SETTING_SECTION_END
+
+
+const gpt35Url = `https://huggingface.co/spaces/kunishou/Rapid-GPT`;
+jsDialog.webView(gpt35Url);
+
