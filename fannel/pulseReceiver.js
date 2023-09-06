bd75584535a4abb21725434c49f240cc23d04767
puutaro <hoperstep@gmail.com>
2023-09-06 22:53:36 +0900

    Add pulse audio receiver

---
+++ fannel/pulseReceiver.js
@@ -0,0 +1,80 @@
+
+
+/// LABELING_SECTION_START
+// Pulse audio reciever @puutaro
+
+// ## Cmd Variables
+// --------
+// ### pcIpAddress
+// pc's ip address
+
+// ### PLAY
+//  start pluse reciever server
+
+// ### STOP
+//  stop pluse reciever server
+
+/// LABELING_SECTION_END
+
+
+/// SETTING_SECTION_START
+editExecute="ALWAYS"
+setReplaceVariables="TXT_LABEL=label"
+setReplaceVariables="BTN_CMD=cmd"
+setReplaceVariables="BTN_LABEL=label"
+setReplaceVariables="LIST_PATH=listPath"
+setReplaceVariables="LIMIT_NUM=limitNum"
+setReplaceVariables="pulseReceiverDirPath=${01}/${001}"
+setReplaceVariables="pulseReceiverListDirPath=${pulseReceiverDirPath}/list"
+setReplaceVariables="pulseReceiverAddressListFilePath=${pulseReceiverListDirPath}/addressList.txt"
+setReplaceVariables="pulseReceiverPortListFilePath=${pulseReceiverListDirPath}/portList.txt"
+setVariableTypes="pcIpAddress:LBL:TXT:ELSB=${TXT_LABEL}=this|${LIST_PATH}=${pulseReceiverAddressListFilePath}!${LIMIT_NUM}=10"
+// setVariableTypes="serverPort:TXT:LBL:TXT:ELSB=${TXT_LABEL}=this|${LIST_PATH}=${pulseReceiverPortListFilePath}!${LIMIT_NUM}=10"
+setVariableTypes="STOP:BTN:HL=${BTN_CMD}=jsf '${0}' stop !${BTN_LABEL}=this"
+hideSettingVariables="editExecute"
+hideSettingVariables="setReplaceVariables"
+hideSettingVariables="setVariableTypes"
+scriptFileName="pulseReceiver.js"
+/// SETTING_SECTION_END
+
+
+/// CMD_VARIABLE_SECTION_START
+pcIpAddress=""
+STOP=""
+/// CMD_VARIABLE_SECTION_END
+
+
+const stopMode = "stop";
+
+endJudge();
+jsFileSystem.createDir("${pulseReceiverListDirPath}");
+switcher();
+
+
+function endJudge(){
+    if(!pcIpAddress) exitZero();
+};
+
+
+function switcher(){
+    let args = jsArgs.get().split("\t");
+    const firstArgs = args.at(0);
+    switch(firstArgs){
+        case "":
+            startPluseServer();
+            break;
+        case stopMode:
+            jsPulseAudioReceiver.stop();  
+            break;
+    };
+};
+
+function startPluseServer(){
+    jsListSelect.updateListFileCon(
+        "${pulseReceiverAddressListFilePath}",
+        pcIpAddress
+    );
+    jsPulseAudioReceiver.start(
+        pcIpAddress
+    );
+};
