2893f22456620dcd60673a4ec0ca39149e6c2d8b
puutaro <55217593+puutaro@users.noreply.github.com>
2023-10-30 15:16:51 +0900

    Update copyLink.js

--- fannel/copyLink.js
+++ fannel/copyLink.js
@@ -14,14 +14,14 @@
 
 /// LABELING_SECTION_END
 
-const LONG_PRESS_NEWS_URL = "CMDCLICK_LONG_PRESS_LINK_URL";
+const LONG_PRESS_LINK_URL = "CMDCLICK_LONG_PRESS_LINK_URL";
 
 end_judge();
-jsUtil.copyToClipboard(LONG_PRESS_NEWS_URL, 10);
-jsToast.short(`Copy ok, ${LONG_PRESS_NEWS_URL}`);
+jsUtil.copyToClipboard(LONG_PRESS_LINK_URL, 10);
+jsToast.short(`Copy ok, ${LONG_PRESS_LINK_URL}`);
 
 function end_judge(){
 	if(
-		!LONG_PRESS_NEWS_URL.includes("/")
+		!LONG_PRESS_LINK_URL.includes("/")
 	) exitZero();
 };
