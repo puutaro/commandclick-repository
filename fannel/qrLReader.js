1267e5d67cbc5bf8f0e1068458ed03d69a59c8d2
puutaro <hoperstep@gmail.com>
2023-12-03 15:21:58 +0900

    Add qrLReader

---
+++ fannel/qrLReader.js
@@ -0,0 +1,75 @@
+
+
+/// LABELING_SECTION_START
+// Scan qr code on long press @puutaro
+// ## Support long press menu
+// ---------
+
+// | type | enable |
+// | ----- | ----- |
+// | src anchor | x |
+// | src image anchor | o |
+// | image | o |
+
+/// LABELING_SECTION_END
+
+
+/// SETTING_SECTION_START
+onUpdateLastModify="OFF"
+onUrlHistoryRegister="OFF"
+scriptFileName="qrLReader.js"
+/// SETTING_SECTION_END
+
+
+/// Please write bellow with javascript
+
+
+const targetUrl = "CMDCLICK_LONG_PRESS_LINK_URL";
+
+
+
+const cmdClickRootDir = "${00}";
+const tempDownloadDirPath = 
+	`${cmdClickRootDir}/temp/download`;
+
+viewForMenu();
+
+function viewForMenu(){
+
+	const cmdclickLongPressImageUrlStr = "CMDCLICK_ENCRPT_LONG_PRESS_IMAGE_URL".replace(
+		"_ENCRPT",
+		""
+	);
+	const cmdclickLongPressLinkUrl = 
+		"CMDCLICK_LONG_PRESS_IMAGE_URL";
+	if(
+		cmdclickLongPressLinkUrl == cmdclickLongPressImageUrlStr
+	) {
+		jsToast.short("Launch from long press");
+		exitZero();
+	};
+	jsToast.short("scan..");
+	const downloadQrImageName = getQrImageAndPath(
+		cmdclickLongPressLinkUrl
+	);
+	const downloadImagePath = `${tempDownloadDirPath}/${downloadQrImageName}`;
+	jsQr.scanConfirmHandler(downloadImagePath);
+	exitZero();
+};
+
+
+function getQrImageAndPath(
+	urlStr,
+){
+	jsCurl.getImage(
+		urlStr
+	);
+	const downloadQrImageName = jsFileSystem.showFileList(
+		tempDownloadDirPath
+	).split("\t").at(0);
+	if(!downloadQrImageName) {
+		alert("no exist");
+		exitZero();
+	};
+	return downloadQrImageName;
+};
