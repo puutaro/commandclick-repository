1f647963d0f75ed26bea826e33f46c0ccf18740c
puutaro <puutaro@users.noreply.github.com>
2024-07-26 05:45:21 +0000

    Apply automatic changes

--- fannel/newsSpeecherDir/shell/install.sh
+++ fannel/newsSpeecherDir/shell/install.sh
@@ -4,8 +4,9 @@ readonly CURRENT_DIR_PATH="$(dirname $0)"
 cd "${CURRENT_DIR_PATH}"
 sudo apt-get install -y \
 	translate-shell
-sudo pip3 install \
+sudo pip3 install -U \
 	newspaper3k \
-	langdetect
+	langdetect \
+	lxml_html_clean
 python3 ./py/nltk_downloader.py
 echo "comp installed"
